61360195cb05518dfd24387b3cbab776eb1612c6
waniqq <94157340+waniqq@users.noreply.github.com>
2022-12-21 10:08:28 +0700

    Add files via upload

---
+++ decision tree stuff 2.py
@@ -0,0 +1,9 @@
+Fin = input("Float in water? (Y/N): ")
+if Fin == ("Y") :
+    print("fake")
+else:
+    Fin2 = input("Can scratch glass? (Y/N): ")
+    if Fin2 == ("Y") :
+        print ("Could be real")
+    else :
+        print("Fake")
